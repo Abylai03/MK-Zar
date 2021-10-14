4cc43617de807c921a568fb647a1ce2091ae0c42
Abylay03 <bite2099@gmail.com>
2021-10-15 01:09:43 +0600

    Home Work - 2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const SubZero = {
   name: "Sub-Zero",
   hp: 100,
-  img: "",
+  img: "http://reactmarathon-api.herokuapp.com/assets/subzero.gif",
   weapon: ["kunai", "shuriken"],
   attack: function () {
     console.log(player1.name + " Fight");
@@ -11,31 +11,39 @@ const SubZero = {
 const Scorpion = {
   name: "Scorpion",
   hp: 100,
-  img: "",
+  img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
   weapon: ["kunai", "shuriken"],
   attack: function () {
     console.log(this.name + " Fight");
   },
 };
 
-function createPlayer(cl, name, hp) {
+function createPlayer(selector, object) {
   const player1 = document.createElement("div");
   const progressbar = document.createElement("div");
   const character = document.createElement("div");
-  const life = document.createElement("div");
-  const name = document.createElement("div");
-  const img = document.createElement("img");
+  let life = document.createElement("div");
+  let name = document.createElement("div");
+  let img = document.createElement("img");
   const arenas = document.querySelector(".arenas");
 
-  player1.classList.add("player1");
+  life.style.width = '100%';
+  name.innerText = object.name;
+  name.style.fontSize = '28px';
+  name.style.color = 'white';
+  life.innerText = object.hp;
+  img.src = object.img;
+
+  player1.classList.add(selector);
   progressbar.classList.add("progressbar");
   character.classList.add("character");
 
   character.appendChild(img);
+  character.appendChild(name);
   player1.appendChild(progressbar);
   player1.appendChild(character);
   arenas.appendChild(player1);
 }
 
-createPlayer('player1', 'SCORPION', 50);
-createPlayer('player2', 'SUB-ZERO', 80);
+createPlayer('player1', Scorpion);
+createPlayer('player2', SubZero);
